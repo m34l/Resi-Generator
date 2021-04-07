b9c235a5621e8aced23f4024f76b7c5590ed26a1
m34l <ismazeindy1@gmail.com>
2021-04-07 08:40:03 +0700

    Update resi.php

--- resi.php
+++ resi.php
@@ -5,7 +5,7 @@
  * Special Thanks To : Setya Mickala as SHARE AIRDROP
  */
 awal:
-echo "MADE BY M34L@ISMAILMUHAMMADZEINDY".PHP_EOL." Pake resi2.php kalo mau cookies lebih lama dipake, ganti manual";
+echo "MADE BY M34L@ISMAILMUHAMMADZEINDY".PHP_EOL;
 
 echo "Berapa Banyak? ";
 $count = trim(fgets(STDIN));
@@ -13,15 +13,13 @@ $count = trim(fgets(STDIN));
 $i = 0;
 $j = 0;
 
-$getcookies = Curi('https://www.dhl.com/en/express/tracking.html?AWB=5797030853&brand=DHL', false, false, false, false, true);
-preg_match_all('%set-cookie: (.*?);%', $getcookies[0], $d);$cookies = '';
-for($o = 0; $o < count($d[0]); $o++)$cookies.= $d[1][$o].";";
+
 
 if (!empty($count)) {
 
     while(true){
         $randomAWB = rand(1000000000,9999999999);
-        $check = dhl($randomAWB,$cookies);
+        $check = dhl($randomAWB);
         $json_check = json_decode($check,true);
         //print_r($json_check);
 
@@ -61,7 +59,7 @@ if (!empty($count)) {
 
 
 
-function dhl($awb,$cookies){
+function dhl($awb){
     $ch = curl_init();
 
     curl_setopt($ch, CURLOPT_URL, 'https://www.dhl.com/shipmentTracking?AWB='.$awb.'&countryCode=g0&languageCode=en&_=1617645656661');
@@ -81,7 +79,6 @@ function dhl($awb,$cookies){
     $headers[] = 'Sec-Fetch-Dest: empty';
     $headers[] = 'Referer: https://www.dhl.com/en/express/tracking.html?AWB=3874133863&brand=DHL';
     $headers[] = 'Accept-Language: id,en-US;q=0.9,en;q=0.8,und;q=0.7';
-    $headers[] = 'Cookie: '.$cookies; 
     curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
     
 
@@ -95,52 +92,6 @@ function dhl($awb,$cookies){
     return $result;
 }
 
-function Curi($url, $fields=false, $cookie=false, $httpheader=false, $proxy=false, $encoding=false, $timeout=false, $useragent=false)
-{ 
-    $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_HEADER, 1);
-    if($useragent !== false)
-    {
-        curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
-    }
-    if($fields !== false)
-    { 
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
-    }
-    if($encoding !== false)
-    { 
-        curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
-    }
-    if($cookie !== false)
-    { 
-        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
-        curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
-    }
-    if($httpheader !== false)
-    { 
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $httpheader);
-    }
-    if($proxy !== false)
-    { 
-        curl_setopt($ch, CURLOPT_PROXY, $proxy);
-        curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
-    }
-    if($timeout !== false)
-    { 
-      // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
-      // curl_setopt($ch, CURLOPT_TIMEOUT, 6); //timeout in seconds     
-    }
-    $response = curl_exec($ch);
-    $header = substr($response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-    $body = substr($response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
-    $code=curl_getinfo($ch, CURLINFO_HTTP_CODE);
-    curl_close($ch);
-    return array($header, $body, $code);
-}
+
 
 ?>
