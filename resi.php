6faba19466a74e1347964c3cd7c181af1b0513fc
m34l <ismazeindy1@gmail.com>
2021-04-08 11:07:16 +0700

    Update resi.php

--- resi.php
+++ resi.php
@@ -6,28 +6,30 @@
  */
 awal:
 echo "MADE BY M34L@ISMAILMUHAMMADZEINDY".PHP_EOL;
-
+echo "1. DHL ".PHP_EOL."2. Aliex".PHP_EOL."3. USPS".PHP_EOL.": ";
+$choose = trim(fgets(STDIN));
 echo "Berapa Banyak? ";
 $count = trim(fgets(STDIN));
 
-$i = 0;
-$j = 0;
+$i = 1;
+$j = 1;
 
 
 
 if (!empty($count)) {
 
     while(true){
-        $randomAWB = ''.random(10,0).'';
+        if ($choose == '1') {
+            $randomAWB = ''.random(10,0).'';
         
-        $check = dhl($randomAWB);
-        $json_check = json_decode($check,true);
+        $check = Curi('https://www.dhl.com/shipmentTracking?AWB='.$randomAWB.'&countryCode=g0&languageCode=en&_=1617645656661');
+        $json_check = json_decode($check[1],true);
         //print_r($json_check);
 
         foreach ($json_check as $key => $code){
             if ($key =="results") {
                 foreach ($code as $kode){
-                    if ($i >= $count) {
+                    if ($i > $count) {
                         die("Done!");
                     } else {
                         $status = $kode['delivery']['status'];
@@ -50,8 +52,67 @@ if (!empty($count)) {
 
             }
         }
+        } else if ($choose == '2') {
+            $string = array('LP','UB');
+            $k = array_rand($string);
+			$v = $string[$k];
 
-        
+            $randomAWB1 = 'LP19'.random(7,0).'SG';
+            $check = Curi('https://global.cainiao.com/detail.htm?mailNoList='.$randomAWB1.'&spm=a3708.7860688.0.d01');
+            $getdata =html_entity_decode(getStr($check[1],'<textarea style="display: none;" id="waybill_list_val_box">','</textarea>'));
+           
+            if (strpos($getdata, 'latestTrackingInfo')) {
+                $datanya = json_decode($getdata);
+                $latest = $datanya->data[0]->latestTrackingInfo->desc;
+                $dest = $datanya->data[0]->destCountry;
+                $org = $datanya->data[0]->originCountry;
+                $time = $datanya->data[0]->latestTrackingInfo->time;
+                $awb = $latestinfo = $datanya->data[0]->mailNo;
+
+                    if ($i > $count) {
+                        die("Done!");
+                    } else {
+
+                        echo "\033[32m$i.  VALID | $awb : $latest  : $org : $dest : $time \033[0m\n";
+                        @ob_flush();
+                         flush();
+                        file_put_contents('resi.txt',"VALID | $awb : $latest  : $org : $dest : $time".PHP_EOL,FILE_APPEND);
+                        $i++;
+
+                    }
+
+            } else {
+                echo "\033[31m$j. $randomAWB1 : INVALID AWB\033[0m\n";
+                @ob_flush();
+                flush();
+                $j++;
+
+            }
+        } else if($choose == '3'){
+            $randomAWB2 = 'LX'.random(9,0).'US';
+            $check = Curi('https://tools.usps.com/go/TrackConfirmAction?tRef=fullpage&tLc=2&text28777=&tLabels='.$randomAWB2.'%2C&tABt=false');
+
+            if (!strpos($check[1],'Label Created, not yet in system')){
+                if ($i > $count) {
+                    die("Done!");
+                } else {
+                echo "\033[32m$i.  VALID | $randomAWB2 : USPS \033[0m\n";
+                @ob_flush();
+                flush();
+                file_put_contents('resiusps.txt',"VALID | $randomAWB2 : USPS".PHP_EOL,FILE_APPEND);
+                $i++;
+                }
+            } else {
+                echo "\033[31m$j. $randomAWB2 : INVALID AWB\033[0m\n"; 
+                @ob_flush();
+                flush();
+                $j++;
+            }
+
+          
+        } else {
+            die("Pilih yang bener tolol");
+        }
     }
 
 } else {
@@ -60,38 +121,58 @@ if (!empty($count)) {
 
 
 
-function dhl($awb){
-    $ch = curl_init();
-
-    curl_setopt($ch, CURLOPT_URL, 'https://www.dhl.com/shipmentTracking?AWB='.$awb.'&countryCode=g0&languageCode=en&_=1617645656661');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-    
-    curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
-    $headers = array();
-    $headers[] = 'Authority: www.dhl.com';
-    $headers[] = 'Sec-Ch-Ua: \"Google Chrome\";v=\"89\", \"Chromium\";v=\"89\", \";Not A Brand\";v=\"99\"';
-    $headers[] = 'Accept: application/json, text/javascript, */*; q=0.01';
-    $headers[] = 'X-Requested-With: XMLHttpRequest';
-    $headers[] = 'Sec-Ch-Ua-Mobile: ?0';
-    $headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36';
-    $headers[] = 'Sec-Fetch-Site: same-origin';
-    $headers[] = 'Sec-Fetch-Mode: cors';
-    $headers[] = 'Sec-Fetch-Dest: empty';
-    $headers[] = 'Referer: https://www.dhl.com/en/express/tracking.html?AWB=3874133863&brand=DHL';
-    $headers[] = 'Accept-Language: id,en-US;q=0.9,en;q=0.8,und;q=0.7';
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-    
-
-    
-    $result = curl_exec($ch);
-    if (curl_errno($ch)) { 
-        echo 'Error:' . curl_error($ch);
+function Curi($url, $fields=false, $cookie=false, $httpheader=false, $proxy=false, $encoding=false, $timeout=false, $useragent=false)
+    { 
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        if($useragent !== false)
+        {
+            curl_setopt($ch, CURLOPT_USERAGENT, $useragent);
+        }
+        if($fields !== false)
+        { 
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+        }
+        if($encoding !== false)
+        { 
+            curl_setopt($ch, CURLOPT_ENCODING, 'gzip, deflate');
+        }
+        if($cookie !== false)
+        { 
+            curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie);
+            curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie);
+        }
+        if($httpheader !== false)
+        { 
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $httpheader);
+        }
+        if($proxy !== false)
+        { 
+            curl_setopt($ch, CURLOPT_PROXY, $proxy);
+            curl_setopt($ch, CURLOPT_PROXYTYPE, CURLPROXY_SOCKS5);
+        }
+        if($timeout !== false)
+        { 
+          // curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 0); 
+          // curl_setopt($ch, CURLOPT_TIMEOUT, 6); //timeout in seconds     
+        }
+        $response = curl_exec($ch);
+        $header = substr($response, 0, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+        $body = substr($response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+        $code=curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        curl_close($ch);
+        return array($header, $body, $code);
     }
-    curl_close($ch);
-    
-    return $result;
-}
+function getStr($string,$start,$end){
+        $str = explode($start,$string);
+        $str = explode($end,$str[1]);
+        return $str[0];
+    } 
 function random($length,$a) 
 	{
 		$str = "";
